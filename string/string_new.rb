45221e11e000a6d33c0172e6a14c3e3d2b83a4f9
tbpgr <tbpgr@tbpgr.jp>
2014-12-03 02:01:29 +0900

    String.new

---
+++ string/string_new.rb
@@ -0,0 +1,15 @@
+require 'tbpgr_utils'
+
+bulk_puts_eval binding, <<-EOS
+"hoge" == String.new("hoge")
+"hoge".eql? String.new("hoge")
+"hoge".equal? String.new("hoge")
+String.new.empty?
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
