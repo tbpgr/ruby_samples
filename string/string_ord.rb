59614387b61ad225bedf283829068c0c79ec1859
tbpgr <tbpgr@tbpgr.jp>
2015-02-03 00:17:44 +0900

    :new: add: add String#ord

---
+++ string/string_ord.rb
@@ -0,0 +1,16 @@
+require 'tbpgr_utils'
+
+bulk_puts_eval binding, <<-EOS
+'h'.ord
+'hoge'.ord
+'ほげ'.ord
+EOS
+
+''.ord rescue p $!.message
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
