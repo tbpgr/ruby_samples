6edf177053129b4f25b75fd6d2e9962d2be6c6d8
tbpgr <tbpgr@tbpgr.jp>
2015-02-25 02:40:42 +0900

    :up: add: add String#to_r

---
+++ string/string_to_r.rb
@@ -0,0 +1,14 @@
+require 'tbpgr_utils'
+
+bulk_puts_eval binding, <<-EOS
+'1/2'.to_r
+'2/2'.to_r
+'1/3'.to_r
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
