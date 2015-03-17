b96585405fcfe2ca9df185197a592f915027e2e9
tbpgr <tbpgr@tbpgr.jp>
2015-03-18 00:31:07 +0900

    :up: add: add Struct#values

---
+++ struct/struct_values.rb
@@ -0,0 +1,18 @@
+require 'tbpgr_utils'
+
+Person = Struct.new(:name, :age)
+Dog = Struct.new(:name, :age, :owner)
+tanaka = Person.new("tanaka", 23)
+pochi = Dog.new("pochi", 2, tanaka)
+bulk_puts_eval binding, <<-EOS
+tanaka.values
+pochi.values
+tanaka.values == tanaka.to_a
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
