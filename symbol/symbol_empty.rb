320b008b58f4d2af5a607b0af9ef8f9f0b5e0d05
tbpgr <tbpgr@tbpgr.jp>
2015-03-31 01:07:50 +0900

    :up: add: add Symbol#empty?

---
+++ symbol/symbol_empty.rb
@@ -0,0 +1,14 @@
+require 'tbpgr_utils'
+
+
+bulk_puts_eval binding, <<-EOS
+:hoge.empty?
+:''.empty?
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
