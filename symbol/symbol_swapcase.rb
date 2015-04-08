29d896b3a63e40401d651b5e337bba54f3e4b211
tbpgr <tbpgr@tbpgr.jp>
2015-04-09 01:05:58 +0900

    :up: add: add Symbol#swapcase

---
+++ symbol/symbol_swapcase.rb
@@ -0,0 +1,12 @@
+require 'tbpgr_utils'
+
+bulk_puts_eval binding, <<-EOS
+:HoGe.swapcase
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
