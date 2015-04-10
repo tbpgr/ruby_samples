9c9b90be100584860ada950b16f1bd1ba6d8a420
tbpgr <tbpgr@tbpgr.jp>
2015-04-11 05:00:46 +0900

    :up: add: add Symbol#upcase

---
+++ symbol/symbol_upcase.rb
@@ -0,0 +1,14 @@
+require 'tbpgr_utils'
+
+bulk_puts_eval binding, <<-EOS
+:hOgE.upcase
+:HoGe.upcase
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
+ 
