0986e3580d6ed953e63eb888cabff27b719f3182
tbpgr <tbpgr@tbpgr.jp>
2015-04-12 06:16:18 +0900

    :up: add: add Time#at

---
+++ time/time_at.rb
@@ -0,0 +1,13 @@
+require 'tbpgr_utils'
+# require 'time'
+
+bulk_puts_eval binding, <<-EOS
+Time.at(Time.now)
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
