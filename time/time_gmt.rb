1cdb8b9e769fb2ab39057e58d2b7c3c63092c236
tbpgr <tbpgr@tbpgr.jp>
2015-04-26 00:15:02 +0900

    :up: add: add Time#gmt?

---
+++ time/time_gmt.rb
@@ -0,0 +1,17 @@
+require 'tbpgr_utils'
+
+day = Time.local(2015,1,1,20,15,1)
+bulk_puts_eval binding, <<-EOS
+day.gmt?
+day
+y = day.getutc
+y.gmt?
+day == y
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
