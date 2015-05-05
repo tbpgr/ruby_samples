06000c534951c2a7c05d519700b32ff3736a4280
tbpgr <tbpgr@tbpgr.jp>
2015-05-06 00:31:48 +0900

    :up: add: add Time#saturday?

---
+++ time/time_saturday.rb
@@ -0,0 +1,17 @@
+require 'tbpgr_utils'
+
+sunday = Time.utc(2015, 1, 4)
+saturday = Time.utc(2015, 1, 3)
+bulk_puts_eval binding, <<-EOS
+sunday.wday
+sunday.saturday?
+saturday.wday
+saturday.saturday?
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
