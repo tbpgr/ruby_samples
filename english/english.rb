c05c2b76a7def93b3d9148e8a4e15143f0ee38f1
tbpgr <tbpgr@tbpgr.jp>
2015-05-28 06:13:43 +0900

    :up: add: add english

---
+++ english/english.rb
@@ -0,0 +1,14 @@
+require 'english'
+require 'tbpgr_utils'
+
+bulk_puts_eval binding, <<-EOS
+$PROGRAM_NAME
+$0
+EOS
+
+__END__
+下記はTbpgrUtils gemの機能
+bulk_puts_eval
+
+https://rubygems.org/gems/tbpgr_utils
+https://github.com/tbpgr/tbpgr_utils
